05d6e4e46642ca7d111fc3f1b1805ddb58fe0a36
Richard Skumat <gitlab@richardskumat.com>
2019-07-29 01:52:44 +0100

    Publish this role, though I don't even use it myself atm

---
+++ add_remotes_back.sh
@@ -0,0 +1,3 @@
+git remote add github git@github:richardskumat/ansible-role-generic_iptables
+git remote add gitlab git@gitlab:richardskumat/ansible-role-generic_iptables
+git remote add origin git@wsgitlab:qwe/ansible-role-generic_iptables
---
+++ readme.md
@@ -0,0 +1,5 @@
+# generic-iptables
+
+generic-iptables role for ansible
+
+I don't recommend anyone to use this role.
